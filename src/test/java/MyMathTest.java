4782c75d3bdda91348863594a34bc4d8f3784041
Franco Horita <franco.horita@gmail.com>
2019-06-19 15:12:23 -0300

    Adding annotations

--- src/test/java/MyMathTest.java
+++ src/test/java/MyMathTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,13 +6,35 @@ public class MyMathTest {
 
     private MyMath myMath = new MyMath();
 
+    @Before
+    public void before(){
+        System.out.println("Before");
+    }
+
+    @After
+    public void after(){
+        System.out.println("After");
+    }
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("After Class");
+    }
+
     @Test
     public void sumWith3Numbers(){
+        System.out.println("sumWith3Numbers");
         assertEquals(6, myMath.sum(new int[]{1, 2, 3}));
     }
 
     @Test
     public void sumWith1Number(){
+        System.out.println("sumWith1Number");
         assertEquals(3, myMath.sum(new int[]{3}));
     }
 
